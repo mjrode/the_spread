45eaae7fb4039e4e0b7b14a0e63208537d6bb638
Michael Rode <michaelrode44@gmail.com>
2017-02-11 12:38:25 -0500

    Created game model and game persistence module.

---
+++ lib/game_data.ex
@@ -0,0 +1,30 @@
+defmodule TheSpread.GameData do
+  import Ecto
+  alias TheSpread.Repo
+  alias TheSpread.Game
+
+  # Game Functions
+  def list_games() do
+    Repo.all(Game)
+  end
+
+  def create_game(game_params) do
+    Beneficiary.changeset(game_params)
+      |> Repo.insert
+  end
+
+  def get_game(id) do
+    Repo.get!(Game, id)
+  end
+
+  def update_game(id, game_params) do
+    Repo.get!(Game, id)
+      |> Beneficiary.changeset(game_params)
+      |> Repo.update
+  end
+
+  def delete_game(id) do
+    Repo.get!(Game, id)
+      |> Repo.delete!
+  end
+end
