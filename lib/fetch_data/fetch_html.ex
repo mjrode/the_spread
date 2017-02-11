db1b747e3e9235ace3969a96704420f6b7334abf
Michael Rode <michaelrode44@gmail.com>
2017-02-11 11:36:26 -0500

    Refactor FetchHTML module to return HTML source using Hound.

--- lib/fetch_data/fetch_html.ex
+++ lib/fetch_data/fetch_html.ex
@@ -1,21 +1,21 @@
-defmodule TheSpread.FetchURL do
+defmodule TheSpread.FetchHTML do
+  use Hound.Helpers
   @moduledoc """
-    Provides a function `massey/2` that returns the proper url for that
+    Provides a function `massey/2` that returns the html source code for that
     sport on that date.
   """
-  @base_url "http://www.masseyratings.com/"
 
   @doc """
-    Returns a massey_url
+    Returns a string of HTML corresponding to that sport and date
 
-    ## Parameters
+    ##Parameters
       - sport: String that represents the sport you want massey_data for
       - date: String in the form of "yyyy-mm-dd" that represents the date of the game
-    ## Examples
-      iex> FetchURL.massey("ncaa_basketball", "2017-02-11")
-      "http://www.masseyratings.com/cb/11590/games?dt=20170211"
+    ##Examples
+      iex> FetchHTML.massey("ncaa_basketball", "2017-02-11")
+      "<!DOCTYPE html><html><head> <meta charset=......"
 
-      iex> FetchURL.massey("not_valid_sport", "2017-02-11")
+      iex> FetchHTML.massey("not_valid_sport", "2017-02-11")
       :error, "No match found for not_valid_sport on 2017-02-11"}
   """
   def massey(sport, date) do
@@ -31,18 +31,28 @@ defmodule TheSpread.FetchURL do
   # Fetch massey URL for ncaa_basketball
   defp fetch_ncaa_basketball_url(date) do
     "http://www.masseyratings.com/cb/11590/games?dt=#{massey_formatted_date(date)}"
+    |> fetch_html
   end
 
   defp fetch_nba_url(date) do
     "http://www.masseyratings.com/nba/games?dt=#{massey_formatted_date(date)}"
+    |> fetch_html
   end
 
   defp fetch_nfl_url(date) do
     "http://www.masseyratings.com/nfl/games?dt=#{massey_formatted_date(date)}"
+    |> fetch_html
   end
 
   defp fetch_ncaa_football_url(date) do
     "http://www.masseyratings.com/cf/11604/games?dt=#{massey_formatted_date(date)}"
+    |> fetch_html
+  end
+
+  defp fetch_html(url) do
+    Hound.start_session
+    navigate_to(url)
+    page_source
   end
 
   # Use this in other module
