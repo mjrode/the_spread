bde0487185ab7f74f2e14f055ec834dc7460a0e8
Michael Rode <michaelrode44@gmail.com>
2017-02-12 15:33:05 -0500

    Added methods to fetch over, and spread from separate table.

--- lib/game_data/parse_wunder_data.ex
+++ lib/game_data/parse_wunder_data.ex
@@ -18,8 +18,57 @@ defmodule TheSpread.ParseWunderData do
   def away_team_name(row) do
     Floki.find(row, ".jTip")
       |> Floki.attribute("name")
+      |> List.first
       |> String.split("vs.")
       |> List.first
       |> String.trim
   end
+
+  def home_team_vegas_line(row) do
+    [_, home, _] = get_odds_table(row)
+    Floki.text(home)
+      |> String.split(" ")
+      |> List.first
+      |> String.split("(")
+      |> List.first
+      |> String.trim
+      |> String.to_float
+  end
+
+  def away_team_vegas_line(row) do
+    [_, _, away] = get_odds_table(row)
+    Floki.text(away)
+      |> String.split(" ")
+      |> List.first
+      |> String.split("(")
+      |> List.first
+      |> String.trim
+      |> String.to_float
+  end
+
+  def over_under_vegas_line(row) do
+    [over, _, _] = get_odds_table(row)
+    Floki.text(over)
+      |> String.split(" ")
+      |> List.first
+      |> String.split("O")
+      |> List.last
+      |> String.trim
+      |> String.to_float
+  end
+
+  def get_odds_url(row) do
+    address = Floki.find(row, "a:nth-child(1)")
+      |> Floki.attribute("href")
+      |> List.first
+    "http://www.wunderdog.com#{address}"
+  end
+
+  def get_odds_table(row) do
+    get_odds_url(row)
+      |> TheSpread.HTML.fetch
+      |> Floki.find("table span.checkmark")
+      |> Enum.drop(15)
+  end
+  #  Floki.find(bad, ".jTip") ; returns [] for bad data
 end
--- test/lib/game_data/parse_wunder_data_test.exs
+++ test/lib/game_data/parse_wunder_data_test.exs
@@ -3,9 +3,28 @@ defmodule ParseWunderDataTest do
   use ExUnit.Case
   alias TheSpread.ParseWunderData
 
+  test "return away team name" do
+    row = wunder_row1
+    home_team_name = ParseWunderData.away_team_name(row)
+    assert home_team_name == "Ohio Bobcats"
+  end
+
   test "return home team name" do
-    row = wunder_row_1
+    row = wunder_row1
     home_team_name = ParseWunderData.home_team_name(row)
     assert home_team_name == "Ball State Cardinals"
+  end
+
+  test "get odds url" do
+    row = wunder_row1
+    url = ParseWunderData.get_odds_url(row)
+    assert url == "http://www.wunderdog.com/consensus/viewScoreOdds/event_id/429862.html?width=610"
+  end
+
+  test "return home_team_vegas_line" do
+    row = wunder_row1
+    home_team_vegas_line = ParseWunderData.home_team_vegas_line(row)
+    assert home_team_vegas_line == -1.5
+
   end
 end
