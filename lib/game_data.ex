bf1d18de7faa8a8327baf806fd18720dc9232580
Michael Rode <michaelrode44@gmail.com>
2017-02-12 12:08:25 -0500

    Added function to fetch massey data for given sport and date

--- lib/game_data.ex
+++ lib/game_data.ex
@@ -2,6 +2,9 @@ defmodule TheSpread.GameData do
   import Ecto
   alias TheSpread.Repo
   alias TheSpread.Game
+  alias TheSpread.HTML
+  alias TheSpread.ParseData
+  alias TheSpread.ConstructURL
 
   # Game Functions
   def list_games() do
@@ -11,6 +14,15 @@ defmodule TheSpread.GameData do
   def create_game(game_params) do
     Game.changeset(game_params)
       |> Repo.insert
+  end
+
+  def fetch_and_insert_massey_games(sport, date) do
+    games = ConstructURL.massey(sport, date)
+      |> HTML.fetch
+      |> ParseData.bundle_games(sport, date)
+
+      for game <- games, do: Game.changeset(%Game{}, game)
+        |> Repo.insert
   end
 
   def get_game(id) do
