0c5fe50388bfe417c672fc8b2fcee6d4ce0db13f
Michael Rode <michaelrode44@gmail.com>
2017-02-13 21:15:28 -0500

    Check if current game exists before creating new wunder records.

--- lib/game_data.ex
+++ lib/game_data.ex
@@ -19,13 +19,24 @@ defmodule TheSpread.GameData do
       |> Repo.insert
   end
 
+  @doc """
+    Fetches and stores a collection of games given that sport and date
+
+    ## Parameters
+      - sport: String that represents the sport you want massey_data for
+      - date: String in the form of "yyyy-mm-dd" that represents the date of the game
+    ## Examples
+      - TheSpread.GameData.fetch_and_insert_wunder_games("ncaa_basketball", "2017-02-10")
+  """
   def fetch_and_insert_wunder_games(sport, date) do
+    # Fetch and parse html, bundle games in to list of maps
     games = ConstructURL.wunderdog(sport, date)
       |> HTML.fetch
       |> ParseWunderData.bundle_games(sport, date)
-
-      for game <- games, do: Game.changeset(%Game{}, game)
-        |> Repo.insert
+      
+      for game <- games do
+        find_or_create(game, date)
+      end
   end
 
   def fetch_and_insert_massey_games(sport, date) do
@@ -34,18 +45,16 @@ defmodule TheSpread.GameData do
       |> ParseMasseyData.bundle_games(sport, date)
 
       for game <- games do
-        home_team_name = game.home_team_name
+        home_team_name = TheSpread.TeamName.format_college_name(game.home_team_name)
         date = game.date
         sport = game.sport
-        game = Map.delete(game, :home_team_name)
+        game_params = Map.delete(game, :home_team_name)
           |> Map.delete(:away_team_name)
           |> Map.delete(:date)
           |> Map.delete(:sport)
-
         require IEx; IEx.pry
-        Repo.one(from g in Game, where: like(g.home_team_name,"^home_team_name%") and (g.date == ^date) and (g.sport == ^sport))
-         |> Game.changeset(game)
-         |> Repo.update
+        Repo.one(from g in Game, where: (g.home_team_name == ^home_team_name) and (g.date == ^date) and (g.sport == ^sport))
+         |> Repo.update(game_params)
       end
   end
 
@@ -62,5 +71,26 @@ defmodule TheSpread.GameData do
   def delete_game(id) do
     Repo.get!(Game, id)
       |> Repo.delete!
+  end
+
+  defp find_or_create(game, date) do
+    game_query = game_query(game, date)
+
+    case game_query do
+      nil ->
+        Game.changeset(%Game{}, game)
+          |> Repo.insert()
+      _present ->
+        Game.changeset(game_query, game)
+          |> Repo.update()
+    end
+  end
+
+  defp game_query(game, date) do
+    Game
+      |> where([g], g.home_team_name == ^game.home_team_name)
+      |> where([g], g.away_team_name == ^game.away_team_name)
+      |> where([g], g.date == ^date)
+      |> Repo.one
   end
 end
