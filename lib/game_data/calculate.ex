553016234378e5774bc2a3f9be898518a44b8ebd
Michael Rode <michaelrode44@gmail.com>
2017-02-19 10:28:04 -0500

    Added correct_prediction and public_percentage_on_massey_team to calc.

--- lib/game_data/calculate.ex
+++ lib/game_data/calculate.ex
@@ -6,7 +6,8 @@ defmodule TheSpread.Calculate do
         line_diff: line_diff(game),
         team_to_bet: team_massey_picks(game),
         over_under_diff: over_under_diff(game),
-        public_percentage_on_massey_team: public_percentage_on_massey_team(game)
+        public_percentage_on_massey_team: public_percentage_on_massey_team(game),
+        correct_prediction: correct_prediction(game)
       }
     else
       nil
@@ -20,6 +21,39 @@ defmodule TheSpread.Calculate do
       false
     end
   end
+
+  def correct_prediction(game) do
+    if team_to_bet_home_or_away(game) == "home" do
+      correct_home_prediction(game)
+    else
+      correct_away_prediction(game)
+    end
+  end
+
+  def correct_home_prediction(game) do
+    game.home_team_vegas_line > actual_home_line(game)
+  end
+
+  def correct_away_prediction(game) do
+    game.away_team_vegas_line > actual_away_line(game)
+  end
+
+  def actual_home_line(game) do
+    (game.away_team_final_score - game.home_team_final_score)
+  end
+
+  def actual_away_line(game) do
+    (game.home_team_final_score - game.away_team_final_score)
+  end
+
+  def team_to_bet_home_or_away(game) do
+    if game.team_to_bet == game.away_team_name do
+      "away"
+    else
+      "home"
+    end
+  end
+
 
   def line_diff(game) do
     abs(game.home_team_massey_line - game.home_team_vegas_line)
@@ -40,6 +74,7 @@ defmodule TheSpread.Calculate do
     massey = game.home_team_massey_line
     vegas  = game.home_team_vegas_line
 
+    require IEx; IEx.pry
     cond  do
       (massey >= 0 && vegas >= 0) && (vegas >= massey) -> game.home_team_name
       (massey >= 0 && vegas >= 0) && (vegas <= massey) -> game.away_team_name
